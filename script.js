49f4a7ce7cc5a52c03bc16fde2cf7e677a43713b
Benjamin Porsont <porsont.benjamin@gmail.com>
2022-12-07 15:23:48 +0100

    modif

--- script.js
+++ script.js
@@ -1,52 +1,151 @@
+/******************Varaiables*******************/
+
 let carteVege = document.getElementById("carte-végé");
+let carteSucre = document.getElementById("carte-sucré");
+let carteBouffe = document.getElementById("carte-bouffé");
 let cartes = document.getElementById("cartes");
-let div = document.createElement("div");
-div.setAttribute("class", "newDiv");
+let divVege = document.createElement("div");
+divVege.setAttribute("class", "divVege");
+document.body.appendChild(divVege);
+let divSucre = document.createElement("div");
+divSucre.setAttribute("class", "divSucre");
+document.body.appendChild(divSucre);
+let divBouffe = document.createElement("div");
+divBouffe.setAttribute("class", "divBouffe");
+document.body.appendChild(divBouffe);
+
+/*******************Click Events******************/
 
 document.getElementById("v-plat-1").addEventListener("click", vege1);
 document.getElementById("v-plat-2").addEventListener("click", vege2);
 document.getElementById("v-plat-3").addEventListener("click", vege3);
-// document.getElementById("v-plat-1").addEventListener("click", sucre1);
-// document.getElementById("v-plat-1").addEventListener("click", sucre2);
-// document.getElementById("v-plat-1").addEventListener("click", sucre3);
-// document.getElementById("v-plat-1").addEventListener("click", bouffe1);
-// document.getElementById("v-plat-1").addEventListener("click", bouffe2);
-// document.getElementById("v-plat-1").addEventListener("click", bouffe3);
+document.getElementById("s-plat-1").addEventListener("click", sucre1);
+document.getElementById("s-plat-2").addEventListener("click", sucre2);
+document.getElementById("s-plat-3").addEventListener("click", sucre3);
+document.getElementById("b-plat-1").addEventListener("click", bouffe1);
+document.getElementById("b-plat-2").addEventListener("click", bouffe2);
+document.getElementById("b-plat-3").addEventListener("click", bouffe3);
+document.querySelector(".divVege").addEventListener("click", removeVege);
+document.querySelector(".divSucre").addEventListener("click", removeSucre);
+document.querySelector(".divBouffe").addEventListener("click", removeBouffe);
 
-function remove() {
-  cartes.replaceChild(carteVege, div);
-  document.querySelector("newDiv").addEventListener("click", remove);
-}
+/*****************Display dishes******************/
 
-function cardCoverMobile() {
-  cartes.replaceChild(div, carteVege);
-  div.style.height = "23em";
-  div.style.width = "100%";
-  div.style.backgroundRepeat = "no-repeat";
-  div.style.backgroundSize = "cover";
-  div.style.marginBottom = "2em";
+/***********VEGE***********/
+
+function cardCoverMobileVege() {
+  cartes.replaceChild(divVege, carteVege);
+  divVege.style.height = "23em";
+  divVege.style.width = "100%";
+  divVege.style.backgroundRepeat = "no-repeat";
+  divVege.style.backgroundSize = "cover";
+  divVege.style.marginBottom = "2em";
 }
 
 function vege1() {
   if (window.matchMedia("(max-width: 1023px)").matches) {
-    cardCoverMobile();
-    div.style.backgroundImage = "url(/assets/images/VEGGIE01.png)";
+    cardCoverMobileVege();
+    divVege.style.backgroundImage = "url(/assets/images/VEGGIE01.png)";
   } else {
   }
 }
 
 function vege2() {
   if (window.matchMedia("(max-width: 1023px)").matches) {
-    cardCoverMobile();
-    div.style.backgroundImage = "url(/assets/images/VEGGIE02.png)";
+    cardCoverMobileVege();
+    divVege.style.backgroundImage = "url(/assets/images/VEGGIE02.png)";
   } else {
   }
 }
 
 function vege3() {
   if (window.matchMedia("(max-width: 1023px)").matches) {
-    cardCoverMobile();
-    div.style.backgroundImage = "url(/assets/images/VEGGIE03.png)";
+    cardCoverMobileVege();
+    divVege.style.backgroundImage = "url(/assets/images/VEGGIE03.png)";
   } else {
   }
+}
+
+/***********SUCRE***********/
+
+function cardCoverMobileSucre() {
+  cartes.replaceChild(divSucre, carteSucre);
+  divSucre.style.height = "23em";
+  divSucre.style.width = "100%";
+  divSucre.style.backgroundRepeat = "no-repeat";
+  divSucre.style.backgroundSize = "cover";
+  divSucre.style.marginBottom = "2em";
+}
+
+function sucre1() {
+  if (window.matchMedia("(max-width: 1023px)").matches) {
+    cardCoverMobileSucre();
+    divSucre.style.backgroundImage = "url(/assets/images/SUGAR01.png)";
+  } else {
+  }
+}
+
+function sucre2() {
+  if (window.matchMedia("(max-width: 1023px)").matches) {
+    cardCoverMobileSucre();
+    divSucre.style.backgroundImage = "url(/assets/images/SUGAR02.png)";
+  } else {
+  }
+}
+
+function sucre3() {
+  if (window.matchMedia("(max-width: 1023px)").matches) {
+    cardCoverMobileSucre();
+    divSucre.style.backgroundImage = "url(/assets/images/SUGAR03.png)";
+  } else {
+  }
+}
+
+/***********BOUFFE***********/
+
+function cardCoverMobileBouffe() {
+  cartes.replaceChild(divBouffe, carteBouffe);
+  divBouffe.style.height = "23em";
+  divBouffe.style.width = "100%";
+  divBouffe.style.backgroundRepeat = "no-repeat";
+  divBouffe.style.backgroundSize = "cover";
+  divBouffe.style.marginBottom = "2em";
+}
+
+function bouffe1() {
+  if (window.matchMedia("(max-width: 1023px)").matches) {
+    cardCoverMobileBouffe();
+    divBouffe.style.backgroundImage = "url(/assets/images/JUNK01.png)";
+  } else {
+  }
+}
+
+function bouffe2() {
+  if (window.matchMedia("(max-width: 1023px)").matches) {
+    cardCoverMobileBouffe();
+    divBouffe.style.backgroundImage = "url(/assets/images/JUNK02.png)";
+  } else {
+  }
+}
+
+function bouffe3() {
+  if (window.matchMedia("(max-width: 1023px)").matches) {
+    cardCoverMobileBouffe();
+    divBouffe.style.backgroundImage = "url(/assets/images/JUNK03.png)";
+  } else {
+  }
+}
+
+/***************Undisplay*****************/
+
+function removeVege() {
+  cartes.replaceChild(carteVege, divVege);
+}
+
+function removeSucre() {
+  cartes.replaceChild(carteSucre, divSucre);
+}
+
+function removeBouffe() {
+  cartes.replaceChild(carteBouffe, divBouffe);
 }
